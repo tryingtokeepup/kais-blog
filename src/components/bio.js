8753f800bd8ea075f808cb763ceb1503ce0079a1
tryingtokeepup <kl.hawaii@gmail.com>
2019-02-15 09:05:08 -0800

    i added a doggie pic

--- src/components/bio.js
+++ src/components/bio.js
@@ -47,7 +47,7 @@ function Bio() {
 
 const bioQuery = graphql`
   query BioQuery {
-    avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+    avatar: file(absolutePath: { regex: "/smalldoggie.png/" }) {
       childImageSharp {
         fixed(width: 50, height: 50) {
           ...GatsbyImageSharpFixed

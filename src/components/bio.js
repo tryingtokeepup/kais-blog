52caf55940f212274f3bf811c430ba498a206bb6
Kai Lovingfoss <32802660+tryingtokeepup@users.noreply.github.com>
2020-09-08 17:58:51 -0700

    updating bio to remove twitter handle (someone took it)
    going to go in and fix this completely a little later on; just removing for now.

--- src/components/bio.js
+++ src/components/bio.js
@@ -34,9 +34,7 @@ function Bio() {
               Written by <strong>{author}</strong> who lives and works in San
               Francisco building useful things.
               {` `}
-              <a href={`https://twitter.com/${social.twitter}`}>
-                You should follow him on Twitter
-              </a>
+
             </p>
           </div>
         );
